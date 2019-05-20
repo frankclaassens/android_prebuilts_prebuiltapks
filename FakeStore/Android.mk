03848635248d1f01a126a5bd7aba020186921ff6
frankclaassens <frank.claassens@gmail.com>
2019-05-20 14:16:25 +0100

    changes
    Change-Id: I431a526e10e31369b731cf263cacadc38e168edb

--- FakeStore/Android.mk
+++ FakeStore/Android.mk
@@ -8,13 +8,13 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := fakestore-permissions.xml
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
+# include $(CLEAR_VARS)
+# LOCAL_MODULE := fakestore-permissions.xml
+# LOCAL_MODULE_TAGS := optional
+# LOCAL_MODULE_CLASS := ETC
+# LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
+# LOCAL_SRC_FILES := $(LOCAL_MODULE)
+# include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
@@ -24,7 +24,8 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := privapp-permissions-com.android.vending.xml
-LOCAL_REQUIRED_MODULES := fakestore-permissions.xml
+LOCAL_REQUIRED_MODULES := \
+	privapp-permissions-com.android.vending.xml
+	#fakestore-permissions.xml
 include $(BUILD_PREBUILT)
 
